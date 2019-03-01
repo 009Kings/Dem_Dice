d3d1d4dde448eed8546897f12fbef2787fb231da
009kings <009kings@gmail.com>
2019-02-28 18:09:00 -0800

    sweet logic bro. Nice assets

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,6 @@ import 'dart:math';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -17,31 +16,8 @@ class MyApp extends StatelessWidget {
   }
 }
 
-// class HomePage extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title:Text('Roll the Dice')
-//       ),
-//       body: Column(
-//         children: <Widget>[],
-//       ),
-//     );
-//   }
-// }
-
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -50,68 +26,76 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  Map<String, int> _result = {
+    'd4': 0,
+    'd6': 0,
+    'd8': 0,
+    'd10': 0,
+    'd12': 0,
+    'd20': 0,
+  };
 
-  void _incrementCounter() {
+  void _incrementCounter(int max) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _result['d${max}'] = Random().nextInt(max) + 1;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+
+    Widget _buildDiceRow(int dX) {
+      return Container(
+        child: Row(
+          mainAxisSize: MainAxisSize.max,
+          children: <Widget>[
+            Column(
+              children: <Widget>[
+                Image.asset("assets/d$dX.png", scale: 2.0, width: 64.0, height: 64.0),
+                Text("$dX-sided die")
+              ],
+            ),
+            RaisedButton(
+              onPressed: () => _incrementCounter(dX),
+              textColor: Colors.white,
+              padding: const EdgeInsets.all(0.0),
+              child: Container(
+                decoration: const BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: <Color>[Colors.teal, Colors.blue],
+                  ),
+                ),
+                padding: const EdgeInsets.all(10.0),
+                child: Text('Roll the Dice'),
+              ),
+            ),
+            Text(
+                "${_result['d${dX}']}",
+                style: Theme.of(context).textTheme.display1,
+            ),
+          ],
+        )
+      );
+    }
+
+
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              'eat my farts',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
+            _buildDiceRow(4),
+            _buildDiceRow(6),
+            _buildDiceRow(8),
+            _buildDiceRow(10),
+            _buildDiceRow(12),
+            _buildDiceRow(20),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
